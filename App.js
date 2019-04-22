285d2dcfff8df5cb28c31ffeeeb2cc0e105b378a
Ritchie Einstein <ritchie.einstein@gmail.com>
2019-04-22 23:45:07 +0530

    Calculation logic implemented. Need some fixes for edge cases

--- App.js
+++ App.js
@@ -18,51 +18,129 @@ const instructions = Platform.select({
 
 export default class App extends Component {
 
-  state = {};
+  state = {
+    firstString: null,
+    operand: null,
+    secondString: null,
+    answerString: '0',
+  };
 
   buttonPress = (key) => {
+    console.log(key + " Pressed");
     debugger;
-    console.log(key + "Pressed");
+    if(key == 'c'){
+      this.setState({
+        firstString: null,
+        operand: null,
+        secondString: null,
+        answerString: '0',
+      });
+      return;
+    }
+    if(key != '+' && key != '-' && key != '*' && key != '/' && key != 'c' && key != '='){
+      var ans = null;
+      if(this.state.firstString != null && this.state.secondString != null){
+        this.setState({...this.state, firstString: ''+key, secondString: null, operand: null, answerString: ''+key});
+        return;
+      }
+      if(this.state.operand == null){
+        var fs = '';
+        if(this.state.firstString == null)fs = ''+key;
+        else fs = ''+ this.state.firstString + key;
+        ans = fs;
+        this.setState({...this.state, firstString: fs, answerString: ans});
+        return;
+      }else{
+        var ss = '';
+        if(this.state.secondString == null)ss = ''+key;
+        else ss = '' + this.state.secondString + key;
+        ans = ss;
+        this.setState({...this.state, secondString: ss, answerString: ans});
+        return;
+      }
+    }else{
+      if(this.state.firstString == null){return};
+      if(key == '='){
+        var ans = 0;
+        if(this.state.secondString == null && this.state.firstString == null){
+          ans = 0;
+        }
+        else if(this.state.secondString == null){
+          ans = this.state.firstString;
+        }
+        else{
+          var firstNumber = Number(this.state.firstString);
+          var secondNumber = Number(this.state.secondString);
+          ans = this.calculateValue(this.state.firstString, this.state.secondString, this.state.operand);
+        }
+        this.setState({...this.state, answerString: ''+ans, operand: key, firstString: null, secondString:null});
+        return;
+      }else{
+        if(this.state.operand == '='){
+          this.setState({...this.state, firstString: ''+this.state.answerString, secondString:null, operand: key});
+        }
+        else if(this.state.firstString != null && this.state.secondString != null){
+          var ans = this.calculateValue(this.state.firstString, this.state.secondString, key);
+          this.setState({...this.state, operand: key, firstString: ''+ans, secondString: null});
+          return;
+        }
+        this.setState({...this.state, operand: key});
+      }
+    }
   }
+
+  calculateValue = (val1, val2, op) =>{
+    var firstNumber = Number(val1);
+    var secondNumber = Number(val2);
+    var ans = 0;
+    switch(op){
+        case '+':
+         ans = firstNumber + secondNumber;
+         break;
+        case '-':
+          ans = firstNumber - secondNumber;
+          break;
+        case '*':
+          ans = firstNumber * secondNumber;
+          break;
+        case '/':
+          ans = firstString / secondNumber;
+    }
+    return ans;
+  }
+
   render() {
-    // return (
-    //   <View style={styles.container}>
-    //     <Text style={styles.welcome}>Welcome to React Native!</Text>
-    //     <Text style={styles.instructions}>To get started, edit App.js</Text>
-    //     <Text style={styles.instructions}>{instructions}</Text>
-    //   </View>
-    // );
     return (
       <View style={{flex: 1, flexDirection: 'column'}}>
-        <View style={styles.resultContainer}><Text style={styles.resultViewText}>Answer</Text></View>
+        <View style={styles.resultContainer}><Text style={styles.resultViewText}>{this.state.answerString}</Text></View>
         <View style={styles.buttonRowContainer}>
-          <TouchableNativeFeedback style={styles.buttonView} onPress={this.buttonPress('c')} underlayColor="white"><View style={styles.buttonView}><Text style={styles.buttonText}>C</Text></View></TouchableNativeFeedback>
-          <TouchableNativeFeedback style={styles.buttonView} onPress={this.buttonPress('c')} underlayColor="white"><View style={styles.buttonView}><Text style={styles.buttonText}>+/-</Text></View></TouchableNativeFeedback>
-          <TouchableNativeFeedback style={styles.buttonView} onPress={this.buttonPress('c')} underlayColor="white"><View style={styles.buttonView}><Text style={styles.buttonText}>%</Text></View></TouchableNativeFeedback>
-          <TouchableNativeFeedback style={styles.buttonView} onPress={this.buttonPress('c')} underlayColor="white"><View style={styles.buttonView}><Text style={styles.buttonText}>/</Text></View></TouchableNativeFeedback>
+          <TouchableNativeFeedback style={styles.buttonView} onPress={()=>this.buttonPress('c')} underlayColor="white"><View style={styles.buttonView}><Text style={styles.buttonText}>C</Text></View></TouchableNativeFeedback>
+          <TouchableNativeFeedback style={styles.buttonView} onPress={()=>this.buttonPress('c')} underlayColor="white"><View style={styles.buttonView}><Text style={styles.buttonText}>+/-</Text></View></TouchableNativeFeedback>
+          <TouchableNativeFeedback style={styles.buttonView} onPress={()=>this.buttonPress('c')} underlayColor="white"><View style={styles.buttonView}><Text style={styles.buttonText}>%</Text></View></TouchableNativeFeedback>
+          <TouchableNativeFeedback style={styles.buttonView} onPress={()=>this.buttonPress('c')} underlayColor="white"><View style={styles.buttonView}><Text style={styles.buttonText}>/</Text></View></TouchableNativeFeedback>
         </View>
         <View style={styles.buttonRowContainer}>
-          <View style={{flex:1, borderRightWidth: 1, borderColor: '#4d4d4d', backgroundColor: '#d9d9d9'}}><Text style={styles.buttonText}>7</Text></View>
-          <View style={{flex:1, borderRightWidth: 1, borderColor: '#4d4d4d', backgroundColor: '#d9d9d9'}}><Text style={styles.buttonText}>8</Text></View>
-          <View style={{flex:1, borderRightWidth: 1, borderColor: '#4d4d4d', backgroundColor: '#d9d9d9'}}><Text style={styles.buttonText}>9</Text></View>
-          <View style={styles.buttonView}><Text style={styles.buttonText}>X</Text></View>
+          <TouchableNativeFeedback onPress={()=>this.buttonPress('7')} underlayColor="white"><View style={{flex:1, borderRightWidth: 1, borderColor: '#4d4d4d', backgroundColor: '#d9d9d9'}}><Text style={styles.buttonText}>7</Text></View></TouchableNativeFeedback>
+          <TouchableNativeFeedback onPress={()=>this.buttonPress('8')} underlayColor="white"><View style={{flex:1, borderRightWidth: 1, borderColor: '#4d4d4d', backgroundColor: '#d9d9d9'}}><Text style={styles.buttonText}>8</Text></View></TouchableNativeFeedback>
+          <TouchableNativeFeedback onPress={()=>this.buttonPress('9')} underlayColor="white"><View style={{flex:1, borderRightWidth: 1, borderColor: '#4d4d4d', backgroundColor: '#d9d9d9'}}><Text style={styles.buttonText}>9</Text></View></TouchableNativeFeedback>
+          <TouchableNativeFeedback onPress={()=>this.buttonPress('*')} underlayColor="white"><View style={styles.buttonView}><Text style={styles.buttonText}>X</Text></View></TouchableNativeFeedback>
         </View>
         <View style={styles.buttonRowContainer}>
-          <View style={{flex:1, borderRightWidth: 1, borderColor: '#4d4d4d', backgroundColor: '#d9d9d9'}}><Text style={styles.buttonText}>4</Text></View>
-          <View style={{flex:1, borderRightWidth: 1, borderColor: '#4d4d4d', backgroundColor: '#d9d9d9'}}><Text style={styles.buttonText}>5</Text></View>
-          <View style={{flex:1, borderRightWidth: 1, borderColor: '#4d4d4d', backgroundColor: '#d9d9d9'}}><Text style={styles.buttonText}>6</Text></View>
-          <View style={styles.buttonView}><Text style={styles.buttonText}>-</Text></View>
+          <TouchableNativeFeedback onPress={()=>this.buttonPress('4')} underlayColor="white"><View style={{flex:1, borderRightWidth: 1, borderColor: '#4d4d4d', backgroundColor: '#d9d9d9'}}><Text style={styles.buttonText}>4</Text></View></TouchableNativeFeedback>
+          <TouchableNativeFeedback onPress={()=>this.buttonPress('5')} underlayColor="white"><View style={{flex:1, borderRightWidth: 1, borderColor: '#4d4d4d', backgroundColor: '#d9d9d9'}}><Text style={styles.buttonText}>5</Text></View></TouchableNativeFeedback>
+          <TouchableNativeFeedback onPress={()=>this.buttonPress('6')} underlayColor="white"><View style={{flex:1, borderRightWidth: 1, borderColor: '#4d4d4d', backgroundColor: '#d9d9d9'}}><Text style={styles.buttonText}>6</Text></View></TouchableNativeFeedback>
+          <TouchableNativeFeedback onPress={()=>this.buttonPress('-')} underlayColor="white"><View style={styles.buttonView}><Text style={styles.buttonText}>-</Text></View></TouchableNativeFeedback>
         </View>
         <View style={styles.buttonRowContainer}>
-          <View style={{flex:1, borderRightWidth: 1, borderColor: '#4d4d4d', backgroundColor: '#d9d9d9'}}><Text style={styles.buttonText}>1</Text></View>
-          <View style={{flex:1, borderRightWidth: 1, borderColor: '#4d4d4d', backgroundColor: '#d9d9d9'}}><Text style={styles.buttonText}>2</Text></View>
-          <View style={{flex:1, borderRightWidth: 1, borderColor: '#4d4d4d', backgroundColor: '#d9d9d9'}}><Text style={styles.buttonText}>3</Text></View>
-          <View style={styles.buttonView}><Text style={styles.buttonText}>+</Text></View>
+          <TouchableNativeFeedback onPress={()=>this.buttonPress('1')} underlayColor="white"><View style={{flex:1, borderRightWidth: 1, borderColor: '#4d4d4d', backgroundColor: '#d9d9d9'}}><Text style={styles.buttonText}>1</Text></View></TouchableNativeFeedback>
+          <TouchableNativeFeedback onPress={()=>this.buttonPress('2')} underlayColor="white"><View style={{flex:1, borderRightWidth: 1, borderColor: '#4d4d4d', backgroundColor: '#d9d9d9'}}><Text style={styles.buttonText}>2</Text></View></TouchableNativeFeedback>
+          <TouchableNativeFeedback onPress={()=>this.buttonPress('3')} underlayColor="white"><View style={{flex:1, borderRightWidth: 1, borderColor: '#4d4d4d', backgroundColor: '#d9d9d9'}}><Text style={styles.buttonText}>3</Text></View></TouchableNativeFeedback>
+          <TouchableNativeFeedback onPress={()=>this.buttonPress('+')} underlayColor="white"><View style={styles.buttonView}><Text style={styles.buttonText}>+</Text></View></TouchableNativeFeedback>
         </View>
         <View style={styles.buttonRowContainer}>
-          <View style={{flex:2, borderRightWidth: 1, borderColor: '#4d4d4d', backgroundColor: '#d9d9d9'}}><Text style={styles.buttonText}>0</Text></View>
-          <View style={{flex:1, borderRightWidth: 1, borderColor: '#4d4d4d', backgroundColor: '#d9d9d9'}}><Text style={styles.buttonText}>.</Text></View>
-          <View style={styles.buttonView}><Text style={styles.buttonText}>=</Text></View>
+          <TouchableNativeFeedback onPress={()=>this.buttonPress('0')} underlayColor="white"><View style={{flex:2, borderRightWidth: 1, borderColor: '#4d4d4d', backgroundColor: '#d9d9d9'}}><Text style={styles.buttonText}>0</Text></View></TouchableNativeFeedback>
+          <TouchableNativeFeedback onPress={()=>this.buttonPress('.')} underlayColor="white"><View style={{flex:1, borderRightWidth: 1, borderColor: '#4d4d4d', backgroundColor: '#d9d9d9'}}><Text style={styles.buttonText}>.</Text></View></TouchableNativeFeedback>
+          <TouchableNativeFeedback onPress={()=>this.buttonPress('=')} underlayColor="white"><View style={styles.buttonView}><Text style={styles.buttonText}>=</Text></View></TouchableNativeFeedback>
         </View>
       </View>
     );
